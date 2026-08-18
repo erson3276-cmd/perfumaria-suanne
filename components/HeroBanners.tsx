de8f16306e4dc3c3d4a66d03ebea9b62c49ace1b
Dev Moca <erson3276@gmail.com>
2026-08-18 15:14:59 -0300

    fix: banner aspect ratio 3/2 mobile, 16/10 desktop

--- components/HeroBanners.tsx
+++ components/HeroBanners.tsx
@@ -37,7 +37,7 @@ export default function HeroBanners() {
 
   return (
     <section className="relative w-full overflow-hidden bg-ink">
-      <div className="relative aspect-[16/9] w-full sm:aspect-[21/9]">
+      <div className="relative aspect-[3/2] w-full sm:aspect-[16/10]">
         {banners.map((b, idx) => (
           <div
             key={b.key}
